aff056129cbdb3f2b5358d78a8aba43caf5f4c38
singh-ashish08 <ashish.rathore.1997@gmail.com>
2025-07-09 07:37:47 +0530

    added delete and ok

--- src/main/java/com/emp/Delete.java
+++ src/main/java/com/emp/Delete.java
@@ -1,5 +1,7 @@
 package com.emp;
 
 public class Delete {
-
+	public static void main(String[] args) {
+		System.out.println("Dont commit this file");
+	}
 }
--- src/main/java/com/emp/security/SecurityConfig.java
+++ src/main/java/com/emp/security/SecurityConfig.java
@@ -1,50 +1,40 @@
 package com.emp.security;
 
-import org.springframework.context.annotation.Configuration;
-
 //@EnableWebSecurity
-@Configuration
+//@Configuration
 public class SecurityConfig {
-<<<<<<< Updated upstream
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public SecurityFilterChain filter(HttpSecurity http) throws Exception {
-
-		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
-				// ⬅️ Allow public access (no login)
-				.requestMatchers("/", "/convert-pdf", "/download/**", "/css/**", "/js/**").permitAll()
-				// 🔐 ADMIN-only access
-				.requestMatchers("/employee/**", "/employee/create", "/employee/update/**", "/employee/pupdate/**",
-						"/employee/delete/**")
-				.hasRole("ADMIN")
-
-				.anyRequest().authenticated())
-
-				.formLogin(Customizer.withDefaults()).logout(logout -> logout.logoutUrl("/logout"))
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.httpBasic(Customizer.withDefaults());
-
-		return http.build();
-	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user1 = User.builder().username("@Ashish08").password(passwordEncoder().encode("!Ashish"))
-				.roles("USER").build();
-		UserDetails user2 = User.builder().username("@Ramesh").password(passwordEncoder().encode("R@m")).roles("USER")
-				.build();
-		UserDetails admin = User.builder().username("@Admin").password(passwordEncoder().encode("!@Admin08"))
-				.roles("ADMIN").build();
-
-		return new InMemoryUserDetailsManager(user1, user2, admin);
-	}
-=======
 	/*
+	 * @Bean public PasswordEncoder passwordEncoder() { return new
+	 * BCryptPasswordEncoder(); }
+	 * 
+	 * @Bean public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+	 * 
+	 * http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth // ⬅️
+	 * Allow public access (no login) .requestMatchers("/", "/convert-pdf",
+	 * "/download/**", "/css/**", "/js/**").permitAll() // 🔐 ADMIN-only access
+	 * .requestMatchers("/employee/**", "/employee/create", "/employee/update/**",
+	 * "/employee/pupdate/**", "/employee/delete/**") .hasRole("ADMIN")
+	 * 
+	 * .anyRequest().authenticated())
+	 * 
+	 * .formLogin(Customizer.withDefaults()).logout(logout ->
+	 * logout.logoutUrl("/logout")) .sessionManagement(session ->
+	 * session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	 * .httpBasic(Customizer.withDefaults());
+	 * 
+	 * return http.build(); }
+	 * 
+	 * @Bean public UserDetailsService userDetailsService() { UserDetails user1 =
+	 * User.builder().username("@Ashish08").password(passwordEncoder().encode(
+	 * "!Ashish")) .roles("USER").build(); UserDetails user2 =
+	 * User.builder().username("@Ramesh").password(passwordEncoder().encode("R@m")).
+	 * roles("USER") .build(); UserDetails admin =
+	 * User.builder().username("@Admin").password(passwordEncoder().encode(
+	 * "!@Admin08")) .roles("ADMIN").build();
+	 * 
+	 * return new InMemoryUserDetailsManager(user1, user2, admin); } =======
+	 * 
 	 * @Bean public PasswordEncoder passwordEncoder() { return new
 	 * BCryptPasswordEncoder(); }
 	 * 
@@ -80,7 +70,8 @@ public class SecurityConfig {
 	 * "!@Admin08")) .roles("ADMIN").build();
 	 * 
 	 * return new InMemoryUserDetailsManager(user1, user2, admin); }
+	 * 
+	 * >>>>>>> Stashed changes
 	 */
->>>>>>> Stashed changes
 
 }
